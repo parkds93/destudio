1afefae5673af33802c68bd6d91ad3a1f44120bc
dsP <8757279@naver.com>
2024-05-29 22:36:31 +0900

    destudio Init

---
+++ src/main/java/com/destudio/DestudioApplication.java
@@ -0,0 +1,13 @@
+package com.destudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/destudio/DestudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
